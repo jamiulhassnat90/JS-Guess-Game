a25ca2683efd03361289ea498b815f18a8f5f68c
jamiul <jamiul90@gmail.com>
2022-02-21 15:35:25 +0600

    Testing push on github

--- script.js
+++ script.js
@@ -67,3 +67,21 @@ function checkGuess(){
 	 randomNumber = Math.floor(Math.random()*100)+1;
 	 
  }
+
+ /*function testGame(){
+	guessCount = 1;
+	const resetParas = document.querySelectorAll('.resultParas p');
+	for(const resetPara of resetParas){
+		resetPara.textContent = '';
+	}
+	resetButton.parentNode.removeChild(resetButton);
+	
+	guessField.disabled=false;
+	guessSubmit.disabled=false;
+	guessField.value = '';
+	guessField.focus();
+	
+	lastResult.style.backgroundColor='white';
+	randomNumber = Math.floor(Math.random()*100)+1;
+	
+} */

b01762bd632f481a1fd77f4aa70f782727573ea0
jamiul <jamiul90@gmail.com>
2022-02-21 15:41:35 +0600

    Delete the testing File

--- script.js
+++ script.js
@@ -68,20 +68,3 @@ function checkGuess(){
 	 
  }
 
- /*function testGame(){
-	guessCount = 1;
-	const resetParas = document.querySelectorAll('.resultParas p');
-	for(const resetPara of resetParas){
-		resetPara.textContent = '';
-	}
-	resetButton.parentNode.removeChild(resetButton);
-	
-	guessField.disabled=false;
-	guessSubmit.disabled=false;
-	guessField.value = '';
-	guessField.focus();
-	
-	lastResult.style.backgroundColor='white';
-	randomNumber = Math.floor(Math.random()*100)+1;
-	
-} */
